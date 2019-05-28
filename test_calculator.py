86fbcbbd93527ae5eee74a9b77c5dd12a450871c
Tommy <nguyenminhtri@tee-coin.com>
2019-05-28 22:12:50 +0700

    add more test case

--- test_calculator.py
+++ test_calculator.py
@@ -7,8 +7,11 @@ import calculator
 
 class TestCalculator:
 
-    def test_addition(self):
+    def test_addition1(self):
         assert 4 == calculator.add(2, 2)
+
+    def test_addition2(self):
+        assert 1 == calculator.add(2, -2)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
