9c1bf22ad07a391e047bf3702b23fff56218b652
Tommy <nguyenminhtri@tee-coin.com>
2019-05-28 22:14:21 +0700

    fix test case

--- test_calculator.py
+++ test_calculator.py
@@ -11,7 +11,7 @@ class TestCalculator:
         assert 4 == calculator.add(2, 2)
 
     def test_addition2(self):
-        assert 1 == calculator.add(2, -2)
+        assert 0 == calculator.add(2, -2)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
